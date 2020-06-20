a9f00135f3be10b71d715923f92f78fb583a1f60
Teplov <vladimir.teplov@gmail.com>
2020-06-20 11:28:29 +0300

    add recursion algo

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -10,3 +10,22 @@ func Sum(i []int) int {
 	}
 	return i[0] + Sum(i[1:])
 }
+
+func FindBiggest(i []int, biggest int) int {
+	l := len(i)
+	switch l {
+	case 0:
+		return 0
+	case 1:
+		if i[0] > biggest {
+			return i[0]
+		}
+		return biggest
+	default:
+		biggest = FindBiggest(i[1:], biggest)
+		if i[0] > biggest {
+			return i[0]
+		}
+		return biggest
+	}
+}
--- recursion/recursion_test.go
+++ recursion/recursion_test.go
@@ -18,3 +18,17 @@ func TestSum(t *testing.T) {
 	r = Sum(input)
 	assert.Equal(t, 3, r)
 }
+
+func TestFindBiggest(t *testing.T) {
+	input := []int{}
+	r := FindBiggest(input, 0)
+	assert.Equal(t, 0, r)
+
+	input = []int{2}
+	r = FindBiggest(input, 0)
+	assert.Equal(t, 2, r)
+
+	input = []int{5, 2, 4}
+	r = FindBiggest(input, 0)
+	assert.Equal(t, 5, r)
+}
