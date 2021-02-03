a75cd075971ca74e297b891562151c3f3010f309
vavilen84 <vladimir.teplov@gmail.com>
2021-02-03 10:49:18 +0200

    binary search: add comments

--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -3,24 +3,30 @@ package binary_search
 import "sort"
 
 func SearchInIntSlice(haystack []int, needle int) (result bool, iterationsCount int) {
-	sort.Ints(haystack)
-	lowKey := 0
-	highKey := len(haystack) - 1
+	sort.Ints(haystack) // this algorithm will not work with not sorted list
+
+	lowKey := 0                  // first index
+	highKey := len(haystack) - 1 // last index
+
 	if (haystack[lowKey] > needle) || (haystack[highKey] < needle) {
-		return
+		return // target value is not in the range
 	}
-	for lowKey <= highKey {
+
+	for lowKey <= highKey { // iteratively reduce list
+
 		iterationsCount++
-		mid := (lowKey + highKey) / 2
+
+		mid := (lowKey + highKey) / 2 // middle index
+
 		if haystack[mid] == needle {
-			result = true
+			result = true // we found our value
 			return
 		}
-		if haystack[mid] < needle {
+		if haystack[mid] < needle { // if needle is bigger than middle - we take only part with highest values by increasing lowKey
 			lowKey = mid + 1
 			continue
 		}
-		if haystack[mid] > needle {
+		if haystack[mid] > needle { // if needle is smaller than middle - we take only part with lowest values by decreasing highKey
 			highKey = mid - 1
 		}
 	}
